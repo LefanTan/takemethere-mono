dfc23ede8af6fa7eacf38fc8853ef90fee1eea6a
Lefan <lefantan@hotmail.com>
2022-10-17 14:55:26 -0600

    Install quasar

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -4,16 +4,4 @@
   <div class="bg-blue h-screen w-screen flex items-center">yoo</div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
